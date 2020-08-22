74aad75093e4355f66b5e7eac52af1b98003a428
tlhysf <13beetyousuf@seecs.edu.pk>
2020-08-23 02:18:13 +0500

    Added routes for components, and links for navigation

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Landing from './components/layout/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <div className="container">
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
