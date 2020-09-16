182824e16275986354911376fc751be7492d17cb
tlhysf <13beetyousuf@seecs.edu.pk>
2020-09-16 23:04:34 +0500

    first deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const keys = require('./config/keys');
 const mongoose = require('mongoose');
@@ -38,6 +39,15 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
+// serve static assests if in production
+if (process.env.NODE_ENV === 'production') {
+	// set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
