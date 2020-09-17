46992624704e98b086c769df40e9109feb6bc973
tlhysf <13beetyousuf@seecs.edu.pk>
2020-09-17 16:35:59 +0500

    ...

---
+++ client/build/precache-manifest.356e34dcd92a0226c7d039349d64ed86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05e8a7c920c8c7db43c72fbbbc26e62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52aa74ee446f1cc2cbf7",
+    "url": "/static/css/main.30d41992.chunk.css"
+  },
+  {
+    "revision": "c5da15c55bf2284327f4",
+    "url": "/static/js/2.2768cedb.chunk.js"
+  },
+  {
+    "revision": "306f3bf9d2f1ee8399ad560a7731d0d6",
+    "url": "/static/js/2.2768cedb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52aa74ee446f1cc2cbf7",
+    "url": "/static/js/main.fe68631b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "27af43f3b215334100d130aece5b6252",
+    "url": "/static/media/showcase.27af43f3.jpg"
+  }
+]);
